be6e3379cf44652b3a96e21a047aed82757c8db7
osmandaway <gaygusuzosman@gmail.com>
2022-09-09 15:05:11 +0200

    Course
    /

---
+++ Python in Enterprise/lab02/main_name.py
@@ -0,0 +1,20 @@
+print(f"The value of __name__ is {__name__}")
+
+def step1():
+    print("Executing step1...")
+def step2():
+    print("Executing step2...")
+def step3():
+    print("Executing step3...")
+
+
+step1()
+step2()
+step3()
+
+import sys
+def main(args):
+    rint(args)
+
+if __name__ == "__main__":
+    main(sys.argv)
